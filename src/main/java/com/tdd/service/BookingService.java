7c8c3845a9e44665e015de9d7f99d1b229b2f07f
lauromiottojr <junior.miotto36@gmail.com>
2022-11-22 16:52:38 -0300

    Criando BookingService

---
+++ src/main/java/com/tdd/service/BookingService.java
@@ -0,0 +1,12 @@
+package com.tdd.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+
+	public Integer daysCalculatorWithDB(String name) {
+		return 10;
+	}
+
+}
