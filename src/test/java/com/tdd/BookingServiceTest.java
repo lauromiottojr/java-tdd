f72ec864bfe9e34cd5d0146c5a3847a6c6e6f7ec
lauromiottojr <junior.miotto36@gmail.com>
2022-11-22 16:58:16 -0300

    Primeiro teste com sucesso

--- src/test/java/com/tdd/BookingServiceTest.java
+++ src/test/java/com/tdd/BookingServiceTest.java
@@ -4,12 +4,24 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.tdd.service.BookingService;
 
 @RunWith(SpringRunner.class)
 public class BookingServiceTest {
+
+	@TestConfiguration
+	static class BookingServiceTestConfig {
+		
+		@Bean
+		public BookingService bookingService() {
+			return new BookingService();
+		}
+		
+	}
 
 	@Autowired
 	private BookingService bookingService;
@@ -18,7 +30,7 @@ public class BookingServiceTest {
 	public void bookingTestServiceDaysCalculator() {
 		String name = "Lauro";
 		Integer days = bookingService.daysCalculatorWithDB(name);
-		
+
 		Assertions.assertEquals(days, 10);
 	}
 
