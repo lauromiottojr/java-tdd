bce41e278446cd42c9ea574348f5af583907346a
lauromiottojr <junior.miotto36@gmail.com>
2022-11-22 21:10:40 -0300

    Iniciando teste em delete e put

--- src/main/java/com/tdd/controller/BookingController.java
+++ src/main/java/com/tdd/controller/BookingController.java
@@ -3,8 +3,10 @@ package com.tdd.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +27,15 @@ public class BookingController {
 	@PostMapping
 	public ResponseEntity<BookingModel> save(BookingModel bookingModel) {
 		return ResponseEntity.status(HttpStatus.OK).body(bookingModel);
+	}
+
+	@DeleteMapping
+	public void delete() {
+
+	}
+
+	@PutMapping
+	public void put() {
+
 	}
 }
--- src/test/java/com/tdd/BookingControllerTest.java
+++ src/test/java/com/tdd/BookingControllerTest.java
@@ -1,5 +1,6 @@
 package com.tdd;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,6 +29,11 @@ public class BookingControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
+	LocalDate checkIn = LocalDate.parse("2022-11-20");
+	LocalDate checkOut = LocalDate.parse("2022-11-30");
+
+	BookingModel bookingModel = new BookingModel(1, "Lauro", checkIn, checkOut, 2);
+
 	@Test
 	public void bookingTestGetAll() throws Exception {
 		mockMvc.perform(get("/bookings")).andExpect(status().isOk());
@@ -35,13 +41,18 @@ public class BookingControllerTest {
 
 	@Test
 	public void bookingTestSave() throws Exception {
-		LocalDate checkIn = LocalDate.parse("2022-11-20");
-		LocalDate checkOut = LocalDate.parse("2022-11-30");
-
-		BookingModel bookingModel = new BookingModel(1, "Lauro", checkIn, checkOut, 2);
-
 		mockMvc.perform(post("/bookings").contentType("application/json")
 				.content(objectMapper.writeValueAsString(bookingModel))).andExpect(status().isOk());
+	}
+
+	@Test
+	public void bookingTestDelete() throws Exception {
+		mockMvc.perform(delete("/bookings")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void bookingTestPut() throws Exception {
+		mockMvc.perform(delete("/bookings")).andExpect(status().isOk());
 	}
 
 }
